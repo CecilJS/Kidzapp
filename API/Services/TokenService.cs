4c97edb8ca7cf6dc9f41f43602c4578facbed0f1
Cecil Jones <dewdrops1800@outlook.com>
2022-03-31 00:29:33 +0100

    JWT configured

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -9,9 +9,44 @@ namespace API.Services
 {
     public class TokenService
     {
-        public TokenService(UserManager<User> userManager)
+        private readonly IConfiguration _config;
+
+        private readonly UserManager<User> _userManager;
+
+        public TokenService(UserManager<User> userManager, IConfiguration config)
         {
+            _userManager = userManager;
+            _config = config;
             
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.email),
+                new Claim(ClaimTypes.Name, user.UserName),
+
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSettings: TokenKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenIptions = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                SigningCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenIptions);
         }
     }
 }
